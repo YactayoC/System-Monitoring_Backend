c45cd438f644fce24f3cdd91cb17d9960d8ab229
Sebastian Yactayo <syactayocrisostomo@gmail.com>
2022-09-28 14:02:00 -0500

    the base structure was added

---
+++ src/main/java/com/sist_monito_backend/SistMonitoBackendApplication.java
@@ -0,0 +1,11 @@
+package com.sist_monito_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistMonitoBackendApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(SistMonitoBackendApplication.class, args);
+   }
+}
---
+++ src/test/java/com/sist_monito_backend/SistMonitoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist_monito_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistMonitoBackendApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
