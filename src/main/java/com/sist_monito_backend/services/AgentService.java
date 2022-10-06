3baa1091bdfce42d0f553ca20de7f653948bf661
Sebastian Yactayo <syactayocrisostomo@gmail.com>
2022-10-05 21:19:48 -0500

    the implementation of the IAgentService interface was eliminated

--- src/main/java/com/sist_monito_backend/services/AgentService.java
+++ src/main/java/com/sist_monito_backend/services/AgentService.java
@@ -1,6 +1,6 @@
 package com.sist_monito_backend.services;
 
-import com.sist_monito_backend.repositories.IClientRepository;
+import com.sist_monito_backend.repositories.AgentRepository;
 import com.sist_monito_backend.entities.Agent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,31 +9,27 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-public class ClientService implements IClientService {
+public class AgentService {
    @Autowired
-   private IClientRepository clientRepository;
+   private AgentRepository agentRepository;
 
-   @Override
    @Transactional(readOnly = true)
    public List<Agent> findAll() {
-      return (List<Agent>) clientRepository.findAll();
+      return agentRepository.findAll();
    }
 
-   @Override
    @Transactional(readOnly = true)
    public Agent findById(Long id) {
-      return clientRepository.findById(id).orElse(null);
+      return agentRepository.findById(id).orElse(null);
    }
 
-   @Override
    @Transactional()
    public Agent save(Agent client) {
-      return clientRepository.save(client);
+      return agentRepository.save(client);
    }
 
-   @Override
    @Transactional
    public void delete(Long id) {
-      clientRepository.deleteById(id);
+      agentRepository.deleteById(id);
    }
 }
